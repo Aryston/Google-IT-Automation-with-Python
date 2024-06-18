1f540399c42a34894760e5506487f019d5fc7078
Aryston <igor.jagec@gmail.com>
2024-06-18 15:36:13 +0000

    dodane extenzije za idx

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -21,6 +21,11 @@
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       # "vscodevim.vim"
+      "github.vscode-github-actions"
+      "jnoortheen.nix-ide"
+      "mhutchie.git-graph"
+      "ms-python.debugpy"
+      "ms-python.python"
     ];
 
     # Enable previews
