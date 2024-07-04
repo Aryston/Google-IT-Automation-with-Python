73701455b646ac52b4376dc8f7a0f6cd14b5311f
Aryston <igor.jagec@gmail.com>
2024-07-04 20:33:08 +0000

    update module 1 finale

--- Module_01/test.py
+++ Module_01/test.py
@@ -24,3 +24,44 @@ while number <= 100:
     else:
         print(number)
     number += 1  
+
+
+for i in range(1, 101):
+    if i % 2 == 0:
+        print(i)
+print("------------------------------------------------")
+
+
+# Prompt the user to enter a number
+number = input("Enter a number: ")
+
+# Check if the input is an integer (isdigit() is a string method that returns True if all characters in the string are digits, False otherwise)
+
+try:
+    # Attempt to convert the input to an integer
+    number = int(number)
+    if number.isdigit():
+        print("The number {} is an integer.".format(number))
+    else:
+        print("The number {} is not an integer.".format(number))
+except ValueError:
+    # If the conversion fails, check if the input contains letters
+    if number.isalpha():
+        # Print an error message if the input contains letters
+        print("Error: The input must be a number, not letters.")
+    else:
+        # Print an error message if the input contains special characters
+        print("Error: The input must be an integer, not special characters.")
+    # Exit the program
+    exit()
+
+
+
+# Check if the number is odd or even
+if number % 2 == 0:
+    print("The number {} is an even number.".format(number))
+else:
+    print("The number {} is an odd number.".format(number))
+
+
+print("-------------------------------------------------")
