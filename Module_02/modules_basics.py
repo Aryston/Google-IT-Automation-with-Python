8f3179afe6da8f404ca4805dd1dc8aa7bf01c037
Aryston <igor.jagec@gmail.com>
2024-07-05 21:21:36 +0000

    Azuriran modules_pasic.py s listom dostupnih modula u IDX

--- Module_02/modules_basics.py
+++ Module_02/modules_basics.py
@@ -2,5 +2,18 @@
 #import subprocess
 
 import datetime
-import arrow
-import area
+#import arrow
+#import area
+
+
+import sys
+
+# Get a list of available module names
+module_names = list(sys.modules.keys())
+
+# Sort the list alphabetically
+module_names.sort()  
+
+# Print the list in a human-readable format
+for name in module_names:
+    print(name)
