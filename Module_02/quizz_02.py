0eba5352068fb581e75e9e1db4502f269ad68ff0
Aryston <igor.jagec@gmail.com>
2024-07-14 12:57:50 +0000

    Practice Quiz: Managing Files & Directories

--- Module_02/quizz_02.py
+++ Module_02/quizz_02.py
@@ -21,3 +21,38 @@ def create_python_script(filename):
 
 # Example usage
 print(create_python_script("program.py"))
+
+
+import os
+import datetime
+
+def file_date(filename):
+    # Check if the filename is a directory, change the filename if it is
+    if os.path.isdir(filename):
+        filename = "new_file.txt"  # Change to a new filename
+
+    # Create the file in the current directory
+    with open(filename, 'w') as file:
+        pass  # Just creating the file, no need to write anything
+    
+    timestamp = os.path.getmtime(filename)
+    # Convert the timestamp into a readable format, then into a string
+    date = datetime.date.fromtimestamp(timestamp)
+    # Return just the date portion
+    # Hint: how many characters are in “yyyy-mm-dd”? 
+    return ("{:%Y-%m-%d}".format(date))
+
+print(file_date("newfile.txt")) 
+# Should be today's date in the format of yyyy-mm-dd
+
+
+import os
+def parent_directory():
+  # Create a relative path to the parent 
+  # of the current working directory 
+  relative_parent = os.path.join(os.getcwd(), '..')
+
+  # Return the absolute path of the parent directory
+  return os.path.os.path.abspath(relative_parent)
+
+print(parent_directory())
