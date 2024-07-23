0f777b0d6fba0c338f0d34d11a1d0546ace17df4
Aryston <igor.jagec@gmail.com>
2024-07-23 20:06:52 +0000

    regex tuesday 23rd July 2024

--- Module_02/regex_01.py
+++ Module_02/regex_01.py
@@ -17,3 +17,28 @@ def check_aei (text):
 print(check_aei("academia")) # True
 print(check_aei("aerial")) # False
 print(check_aei("paramedic")) # False
+
+print("----------------------")
+import re
+def check_punctuation (text):
+  result = re.search(r"[;',./:?]", text)
+  return result != None
+
+print(check_punctuation("This is a sentence that ends with a period.")) # True
+print(check_punctuation("This is a sentence fragment without a period")) # False
+print(check_punctuation("Aren't regular expressions awesome?")) # True
+print(check_punctuation("Wow! We're really picking up some steam now!")) # True
+print(check_punctuation("End of the line")) # False
+print(re.search(r"cat|dog", "I like dogs")) # <re.Match object; span=(7, 10), match='dog'>
+
+print("----------------------")
+
+import re
+def repeating_letter_a(text):
+  result = re.search(r"[aA].*[aA]", text)
+  return result != None
+
+print(repeating_letter_a("banana")) # True
+print(repeating_letter_a("pineapple")) # False
+print(repeating_letter_a("Animal Kingdom")) # True
+print(repeating_letter_a("A is for apple")) # True
